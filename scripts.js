c633010d445dedb73d77ffb824f20eb8dc2395af
Nsl106 <87742096+Nsl106@users.noreply.github.com>
2023-09-18 16:10:33 -0700

    Cleanup Code

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "325px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
+
+window.addEventListener("scroll", reveal);
